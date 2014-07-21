80a1d0ff0823cb6df0501ea81c10f8660118bf6e
John Banning <jwbanning@gmail.com>
2014-07-21 16:48:56 -0500

    reading the JSON file to show the initial markers.

--- code/assets/map-methods.js
+++ code/assets/map-methods.js
@@ -1,115 +1,101 @@
 $(document).ready(function() {
-  $.getJSON('/assets/best-driver.json', function(data){
-    var test="";
-  });
+  var data ='';
+  // $.getJSON('/assets/best-driver.json', function(data){
+  //   data=data;
+  // });
 
   $(function(){
+    $.getJSON('/assets/best-driver.json', function(data){
+      data=data;
 
+// TABS
+      $( "#tabs .tab" ).click(function(e) {
+        var id = $(e.currentTarget).attr('id');
+        $( "#tabs .tab" ).removeClass('selected');
+        $(e.currentTarget).addClass('selected');
+      });
+// TABS
 
-    $( "#tabs .tab" ).click(function(e) {
-      var id = $(e.currentTarget).attr('id');
-      //clear all the classes on the LI
-      $( "#tabs .tab" ).removeClass('selected');
-      $(e.currentTarget).addClass('selected');
-      
-    });
-
+    var markers = [];
+    for (var i = 0; i < data.length; i++) {
+      // newMarkers.push(data[i].Lat, data[i].Long);
+      markers.push({'latLng': [data[i].Lat, data[i].Lon], 'name': data[i].City +", " + data[i].State});
+    }
 
     var regionStyling = {initial: {fill: '#e8e8e8'},hover: {fill: "#666"}};
-    var markers = [
-          [61.18, -149.53],
-          [21.18, -157.49],
-          {latLng: [40.66, -73.56], name: 'New York City', style: {r: 8, fill: 'yellow'}},
-          {latLng: [41.52, -87.37], style: {fill: 'red', r: 10}}
-        ],
-        values1 = [1, 2, 3, 4],
-        values2 = [1, 2, 3, 4];
+    // var markers = [
+    //       [61.18, -149.53],
+    //       [21.18, -157.49],
+    //       {latLng: [40.66, -73.56], name: 'New York City', style: {r: 8, fill: 'yellow'}},
+    //       {latLng: [41.52, -87.37], style: {fill: 'red', r: 10}}
+    //     ],
+    //     values1 = [1, 2, 3, 4],
+    //     values2 = [1, 2, 3, 4];
 
-    var map = new jvm.WorldMap({
-      container: $('.map'),
-      map: 'us_aea_en',
-      markers: markers,
-      backgroundColor: '#fff',
-      regionStyle:regionStyling,
-      series: {
-        markers: [{
-          attribute: 'fill',
-          scale: ['#C8EEFF', '#0071A4'],
-          normalizeFunction: 'polynomial',
-          values: values1
-        },{
-          attribute: 'r',
-          scale: [5, 20],
-          normalizeFunction: 'polynomial',
-          values: values2
-        }],
-        regions: [{
-          scale: {
-            red: '#ccc',
-            blue: '#999'
-          },
-          attribute: 'fill',
-          normalizeFunction: 'polynomial',
-          values: {
-            "US-KS": 'red',
-            "US-MO": 'red',
-            "US-IA": 'blue',
-            "US-NE": 'blue'
+      var map = new jvm.WorldMap({
+        container: $('.map'),
+        map: 'us_aea_en',
+        backgroundColor: '#fff',
+        regionStyle:regionStyling,
+        markerStyle: {
+          initial: {
+            fill: '#0096d6',
+            stroke: '#0096d6'
           }
-        }]
-      },
-      regionsSelectable: false,
-      markersSelectable: true,
-      markersSelectableOne: true,
-      selectedRegions: JSON.parse( window.localStorage.getItem('jvectormap-selected-regions') || '[]' ),
-      selectedMarkers: JSON.parse( window.localStorage.getItem('jvectormap-selected-markers') || '[]' ),
+        },
+        markers: markers,
+        regionsSelectable: false,
+        markersSelectable: true,
+        markersSelectableOne: true,
+        // selectedRegions: JSON.parse( window.localStorage.getItem('jvectormap-selected-regions') || '[]' ),
+        // selectedMarkers: JSON.parse( window.localStorage.getItem('jvectormap-selected-markers') || '[]' ),
 
-      onMarkerLabelShow: function(event, label, index){
-        label.html(label.html()+' (modified marker)');
-      },
-      onMarkerOver: function(event, index){
-        console.log('marker-over', index);
-      },
-      onMarkerOut: function(event, index){
-        console.log('marker-out', index);
-      },
-      onMarkerClick: function(event, index){
-        console.log('marker-click', index);
-      },
-      onMarkerSelected: function(event, index, isSelected, selectedMarkers){
-        console.log('marker-select', index, isSelected, selectedMarkers);
-        if (window.localStorage) {
-          window.localStorage.setItem(
-            'jvectormap-selected-markers',
-            JSON.stringify(selectedMarkers)
-          );
+        onMarkerLabelShow: function(event, label, index){
+          label.html(label.html()+' (modified marker)');
+        },
+        onMarkerOver: function(event, index){
+          console.log('marker-over', index);
+        },
+        onMarkerOut: function(event, index){
+          console.log('marker-out', index);
+        },
+        onMarkerClick: function(event, index){
+          console.log('marker-click', index);
+        },
+        onMarkerSelected: function(event, index, isSelected, selectedMarkers){
+          console.log('marker-select', index, isSelected, selectedMarkers);
+          if (window.localStorage) {
+            window.localStorage.setItem(
+              'jvectormap-selected-markers',
+              JSON.stringify(selectedMarkers)
+            );
+          }
+        },
+        onRegionLabelShow: function(event, label, code){
+          label.html(label.html()+' (modified)');
+        },
+        onRegionOver: function(event, code){
+          console.log('region-over', code, map.getRegionName(code));
+        },
+        onRegionOut: function(event, code){
+          console.log('region-out', code);
+        },
+        onRegionClick: function(event, code){
+          console.log('region-click', code);
+        },
+        onRegionSelected: function(event, code, isSelected, selectedRegions){
+          console.log('region-select', code, isSelected, selectedRegions);
+          if (window.localStorage) {
+            window.localStorage.setItem(
+              'jvectormap-selected-regions',
+              JSON.stringify(selectedRegions)
+            );
+          }
+        },
+        onViewportChange: function(e, scale, transX, transY){
+            console.log('viewportChange', scale, transX, transY);
         }
-      },
-      onRegionLabelShow: function(event, label, code){
-        label.html(label.html()+' (modified)');
-      },
-      onRegionOver: function(event, code){
-        console.log('region-over', code, map.getRegionName(code));
-      },
-      onRegionOut: function(event, code){
-        console.log('region-out', code);
-      },
-      onRegionClick: function(event, code){
-        console.log('region-click', code);
-      },
-      onRegionSelected: function(event, code, isSelected, selectedRegions){
-        console.log('region-select', code, isSelected, selectedRegions);
-        if (window.localStorage) {
-          window.localStorage.setItem(
-            'jvectormap-selected-regions',
-            JSON.stringify(selectedRegions)
-          );
-        }
-      },
-      onViewportChange: function(e, scale, transX, transY){
-          console.log('viewportChange', scale, transX, transY);
-      }
-
+      });
     });
   });
 });
